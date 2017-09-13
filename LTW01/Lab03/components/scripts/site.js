8d767559bb60470f660aa91c16d29bf7f09a60d9
KennyNguyen88 <giatrangrua@gmail.com>
2017-09-13 21:40:29 +0700

    Submit Lab09 - Jquery

--- LTW01/Lab03/components/scripts/site.js
+++ LTW01/Lab03/components/scripts/site.js
@@ -19,19 +19,49 @@
      */
 
     /* REGISTER */
-    var registerPassword = document.getElementById('inputRegisterPassword');
-    var registerConfirmPassword = document.getElementById('inputRegisterConfirmPassword');
+    // var registerPassword = document.getElementById('inputRegisterPassword');
+    // var registerConfirmPassword = document.getElementById('inputRegisterConfirmPassword');
+    //
+    // var checkPasswordMatch = function(){
+    //     if (registerPassword.value != registerConfirmPassword.value){
+    //         registerConfirmPassword.setCustomValidity('Password must match !');
+    //     }
+    //     else{
+    //         registerConfirmPassword.setCustomValidity('');
+    //     }
+    // };
+    //
+    // registerPassword.onchange = checkPasswordMatch;
+    // registerConfirmPassword.onchange = checkPasswordMatch;
 
-    var checkPasswordMatch = function(){
-        if (registerPassword.value != registerConfirmPassword.value){
-            registerConfirmPassword.setCustomValidity('Password must match !');
-        }
-        else{
-            registerConfirmPassword.setCustomValidity('');
-        }
-    };
-
-    registerPassword.onchange = checkPasswordMatch;
-    registerConfirmPassword.onchange = checkPasswordMatch;
+    $('.sp-wrap').smoothproducts();
 
 }());
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                dots:true,
+                dotsEach:true
+            },
+            600:{
+                items:2,
+                dots:true,
+                dotsEach:true
+            },
+            1000:{
+                items:4,
+                loop:false,
+                dots:true,
+                dotsEach:true
+            }
+        }
+    });
+
+    $('#inputRegisterPassword').pwstrength();
+});
