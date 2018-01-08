daebf747f79db5367c0d703bc8696cb27a37a674
KennyNguyen88 <giatrangrua@gmail.com>
2018-01-08 22:39:12 +0700

    Submit 20180109

---
+++ PTTKPM/1581270_Ban_Tivi_Nhan_Vien_Ban_Hang/Ung_Dung/3-Doi_tuong_va_Xu_ly/3B-Xu_ly_M+.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Helpers;
+using System.Web.Hosting;
+using System.Globalization;
+using System.Net;
+//************************* M+ (Model for All ) **********************************
+public partial class XL_DU_LIEU
+{
+
+    public static XL_DU_LIEU Khoi_dong_Du_lieu_Ung_dung()
+    {
+        var Du_lieu_Ung_dung = Doc_Du_lieu();// Tạm thời chưa Caching
+        
+        return Du_lieu_Ung_dung;
+    }
+}
+//************************* View-Layers/Presentation Layers VL/PL **********************************
+public partial class XL_DU_LIEU
+{    
+    public  string Dia_chi_Media = $"{Dia_chi_Dich_vu}/Media";
+    public  CultureInfo Dinh_dang_VN = CultureInfo.GetCultureInfo("vi-VN");
+
+    public  string Tao_Chuoi_HTML_Danh_sach_Tivi(List<XL_TIVI> Danh_sach)
+    {
+        var Chuoi_HTML_Danh_sach = "<div class='row'>";
+        Danh_sach.ForEach(Tivi =>
+        {
+            var So_luong_Ton = Tinh_So_luong_Ton_Tivi(Tivi);
+            var Doanh_Thu = Tinh_Doanh_Thu(Tivi);
+            var Chuoi_Hinh = $"<img src='{Dia_chi_Media}/{Tivi.Ma_so}.png' " +
+                             "style='width:90px;height:90px;' />";
+            
+            var Chuoi_Thong_tin = $"<div class='btn' style='text-align:left'> " +
+                          $"{Tivi.Ten}" +
+                          $"<br />Đơn giá Bán { Tivi.Don_gia_Ban.ToString("n0", Dinh_dang_VN) }" +
+                           $"<br />Số lượng Tồn { So_luong_Ton.ToString("n0", Dinh_dang_VN)}" +
+                           $"<br />Doanh Thu { Doanh_Thu.ToString("n0", Dinh_dang_VN)}" +
+                          $"</div>";
+            var Chuoi_HTML = $"<div class='col-md-4' style='margin-bottom:10px' >" +
+                               $"{Chuoi_Hinh}" + $"{Chuoi_Thong_tin}" +
+                             "</div>";
+            
+            Chuoi_HTML_Danh_sach += Chuoi_HTML;
+        });
+        Chuoi_HTML_Danh_sach += "</div>";
+        return Chuoi_HTML_Danh_sach;
+    }
+    public string Tao_Chuoi_HTML_Danh_Sach_Nhom_Tivi(List<XL_TIVI> Danh_sach_Tivi, List<XL_NHOM_TIVI> Danh_Sach_Nhom_Tivi)
+    {
+        var Chuoi_HTML = "<div class='row'>";
+        Danh_Sach_Nhom_Tivi.ForEach(nhom =>
+        {
+            var TenNhom = nhom.Ten;
+            var MaSo = nhom.Ma_so;
+            long SoLuongTon = 0;
+            double DoanhThu = 0;
+            Danh_sach_Tivi.ForEach(tivi =>
+            {
+                if (tivi.Nhom_Tivi.Ma_so == MaSo)
+                {
+                    DoanhThu = DoanhThu + Tinh_Doanh_Thu(tivi);
+                    SoLuongTon = SoLuongTon + Tinh_So_luong_Ton_Tivi(tivi);
+                }
+            });
+
+            var Chuoi_Thong_tin = $"<div class='col-12'>Nhóm {TenNhom} | Tồn kho: {SoLuongTon} | Doanh thu: {DoanhThu} </div>";
+
+            Chuoi_HTML += Chuoi_Thong_tin;
+
+        });
+        Chuoi_HTML += "</div>";
+        return Chuoi_HTML;
+    }
+
+    public string Tao_Chuoi_HTML_Danh_Sach_Nhan_Vien_Ban_Hang(List<XL_TIVI> Danh_sach_Tivi, List<XL_NHAN_VIEN> Danh_Sach_Nhan_Vien)
+    {
+
+        var Chuoi_HTML = $"<div class='row'>";
+        Danh_Sach_Nhan_Vien.ForEach(nv =>
+        {
+            var HoTen = nv.Ho_ten;
+            double DoanhThu = 0;
+            List<XL_NHOM_TIVI> Danh_Sach_Nhom_Tivi = nv.Danh_sach_Nhom_Tivi;
+            Danh_sach_Tivi.ForEach(tivi =>
+            {
+                if (Danh_Sach_Nhom_Tivi.FindIndex(nhom => nhom.Ma_so == tivi.Nhom_Tivi.Ma_so) > -1)
+                {
+                    DoanhThu += Tinh_Doanh_Thu(tivi);
+                }
+            });
+            var Chuoi_Thong_Tin = $"<div class='col-12'>Họ tên: {HoTen} | Doanh Thu: {DoanhThu} </div>";
+
+            Chuoi_HTML += Chuoi_Thong_Tin;
+
+        });
+        Chuoi_HTML += "</div>";
+        return Chuoi_HTML;
+
+    }
+
+    public string Tao_Chuoi_HTML_Thong_Tin_Cong_Ty(List<XL_TIVI> Danh_sach_Tivi)
+    {
+        var Chuoi_HTML = "<div class='row'>";
+        double DoanhThu = 0;
+        long SoTon = 0;
+        Danh_sach_Tivi.ForEach(tivi =>
+        {            
+            DoanhThu += Tinh_Doanh_Thu(tivi);
+            SoTon += Tinh_So_luong_Ton_Tivi(tivi);
+        });
+        var Chuoi_Thong_Tin = $"<div class='col-12'>Số lượng tồn: {SoTon} | Doanh Thu: {DoanhThu} </div>";
+        Chuoi_HTML += Chuoi_Thong_Tin;
+        Chuoi_HTML += "</div>";
+        return Chuoi_HTML;
+
+    }
+
+    public string Tao_Chuoi_HTML_Doanh_Thu_Nhan_Vien(List<XL_TIVI> Danh_sach_Tivi, XL_NHAN_VIEN NhanVien)        
+    {
+        var Chuoi_HTML = "<div class='row'>";
+        var TenNhanVien = NhanVien.Ho_ten;
+        double DoanhThu = 0;
+        List<XL_NHOM_TIVI> Danh_Sach_Nhom_Tivi = NhanVien.Danh_sach_Nhom_Tivi;
+        Danh_sach_Tivi.ForEach(tivi =>
+        {
+            if (Danh_Sach_Nhom_Tivi.FindIndex(nhom => nhom.Ma_so == tivi.Nhom_Tivi.Ma_so) > -1)
+            {
+                DoanhThu += Tinh_Doanh_Thu(tivi);
+            }
+        });
+        var Chuoi_Thong_tin = $"<div class='col-12'>Nhân viên:  {TenNhanVien} | Doanh thu: {DoanhThu} </div>";
+        Chuoi_HTML += Chuoi_Thong_tin;
+        Chuoi_HTML += "</div>";
+        return Chuoi_HTML;
+    }
+}
+//************************* Business-Layers BL **********************************
+public partial class XL_DU_LIEU
+{   // Lưu ý PET : Chỉ sử dụng khi đã có Đối tượng Du_lieu ( sau khi khởi động)
+    public   List<XL_TIVI> Tra_cuu_Tivi(
+         string Chuoi_Tra_cuu, List<XL_TIVI> Danh_sach)
+    {
+        Danh_sach = Danh_sach.FindAll(
+              Tivi => Tivi.Ten.ToUpper().Contains(Chuoi_Tra_cuu.ToUpper()));
+        return Danh_sach;
+    }
+    public long Tinh_So_luong_Ton_Tivi(XL_TIVI Tivi)
+    {
+        var Tong_Nhap = Tivi.Danh_sach_Nhap_hang.Sum(Nhap_hang => Nhap_hang.So_luong);
+        var Tong_Ban = Tivi.Danh_sach_Ban_hang.Sum(Ban_hang => Ban_hang.So_luong);
+        var So_luong_Ton = Tong_Nhap - Tong_Ban;
+        return So_luong_Ton;
+    }
+    public double Tinh_Doanh_Thu(XL_TIVI Tivi)
+    {
+        return Tivi.Danh_sach_Ban_hang.Sum(Ban_Hang => Ban_Hang.Tien);
+    }
+
+}
+//************************* Data-Layers DL **********************************
+public partial class XL_DU_LIEU
+{
+    static string Dia_chi_Dich_vu = "http://localhost:50900";
+    static string Dia_chi_Dich_vu_Khach_Tham_quan = $"{Dia_chi_Dich_vu}/1-Dich_vu_Giao_tiep/DV_Chinh.cshtml";
+
+    static XL_DU_LIEU Doc_Du_lieu()
+    {
+         
+        var Xu_ly = new WebClient();
+        Xu_ly.Encoding = System.Text.Encoding.UTF8;
+        var Tham_so = "Ma_so_Xu_ly=DOC_DU_LIEU";
+        var Dia_chi_Xu_ly = $"{Dia_chi_Dich_vu_Khach_Tham_quan}?{Tham_so}";
+        var Chuoi_JSON = Xu_ly.DownloadString(Dia_chi_Xu_ly);
+        var Du_lieu = Json.Decode<XL_DU_LIEU>(Chuoi_JSON);
+        return Du_lieu;
+    }
+   
+
+}
---
+++ PTTKPM/1581270_Ban_Tivi_Quan_Ly_Cua_Hang/1581270_Ban_Tivi_Quan_Ly_Cua_Hang/3-Doi_Tuong_Va_Xu_Ly/3A-Mo_hinh_Doi_tuong.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+public partial class XL_DU_LIEU
+{
+    public XL_CUA_HANG Cua_hang = new XL_CUA_HANG();
+    public List<XL_TIVI> Danh_sach_Tivi = new List<XL_TIVI>();
+}
+public class XL_CUA_HANG
+{
+    public string Ten, Ma_so = "";
+    public List<XL_NHOM_NHAN_VIEN> Danh_sach_Nhom_Nhan_vien = new List<XL_NHOM_NHAN_VIEN>();
+    public List<XL_NHOM_TIVI> Danh_Sach_Nhom_Tivi = new List<XL_NHOM_TIVI>();
+    public List<XL_NHAN_VIEN> Danh_sach_Nhan_vien = new List<XL_NHAN_VIEN>();
+    public List<XL_QUAN_LY> Danh_sach_Quan_ly = new List<XL_QUAN_LY>();
+}
+public class XL_NHOM_TIVI
+{
+    public string Ten, Ma_so = "";
+}
+public class XL_NHOM_NHAN_VIEN
+{
+    public string Ten, Ma_so = "";
+}
+public class XL_NHOM_QUAN_LY
+{
+    public string Ten, Ma_so = "";
+}
+public class XL_NHAN_VIEN
+{
+    public string Ho_ten, Ma_so = "", Ten_Dang_nhap, Mat_khau;
+    public XL_NHOM_NHAN_VIEN Nhom_Nhan_vien = new XL_NHOM_NHAN_VIEN();
+    public List<XL_NHOM_TIVI> Danh_sach_Nhom_Tivi = new List<XL_NHOM_TIVI>();
+}
+public class XL_QUAN_LY
+{
+    public string Ho_ten, Ma_so = "", Ten_Dang_nhap, Mat_khau;
+    public XL_NHOM_QUAN_LY Nhom_Quan_Ly = new XL_NHOM_QUAN_LY();
+}
+public class XL_TIVI
+{
+    public string Ten, Ma_so = "";
+    public long Don_gia_Ban, Don_gia_Nhap;
+    public XL_NHOM_TIVI Nhom_Tivi = new XL_NHOM_TIVI();
+    public List<XL_NHAP_HANG> Danh_sach_Nhap_hang = new List<XL_NHAP_HANG>();
+    public List<XL_BAN_HANG> Danh_sach_Ban_hang = new List<XL_BAN_HANG>();
+}
+public class XL_NHAP_HANG
+{
+    public DateTime Ngay = DateTime.Today;
+    public long Don_gia, So_luong, Tien;
+    public XL_NHAN_VIEN Nhan_vien = new XL_NHAN_VIEN();
+
+}
+public class XL_BAN_HANG
+{
+    public DateTime Ngay = DateTime.Today;
+    public long Don_gia, So_luong, Tien;
+    public XL_NHAN_VIEN Nhan_vien = new XL_NHAN_VIEN();
+
+}
